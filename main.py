a0276c525b215481777dfdb7581c0f1a1c0c3c25
hchanampe <hugogher@gmail.com>
2019-07-16 17:25:18 -0300

    Se han completado algunos metodos

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ class Fila(object):
 
     def __init__(self):
          """constructor de la clase Fila """
-        self.enfila= 0
-        self.fila = []
+         self.enfila=0
+         self.fila=[]
 
 class FilaPreferencial(Fila):
     """Clase de la fila de los clientes preferenciales"""        
 
     def insertar(self, cliente):
         """Inserta un nuevo cliente en la fila preferencial"""
-        pass
+        if cliente.categoria == 'preferencial':
+            self.fila.append(cliente)
+            self.enfila+=1
+        else:
+            print("El cliente no es preferencial")
 
     def atender(self):
         """Atiende al proximo cliente prederencial"""
@@ -29,25 +33,37 @@ class FilaGeneral(Fila):
 
     def insertar(self, cliente):
         """Inserta un nuevo cliente en la fila no preferencial"""
-        pass
+        self.enfila+=1
+        self.fila.append(cliente)
 
     def atender(self):
         """Atiende al proximo cliente prederencial"""
-        pass      
+        self.enfila-=1
+        self.fila.pop(0)
+        
 
     
 
 class cliente(object):
      """clase cliente """
-    def __init__(self,dni):
+     def __init__(self,dni):
          """ constructor de la clase cliente """
-        self.dni=dni
-        self.categoria=None
-    def modificarcategoria(self, categoria):
+         self.dni=dni
+         self.categoria=None
+     def modificarcategoria(self, categoria):
         """modifica el atributo categoria del cliente """
-        pass
-  
+        self.categoria=categoria
+     def getCategoria(self):
+        return self.categoria
+     def __str__(self):
+         return "DNI:"+str(self.dni)+" - Categoria:"+self.categoria
     
 if __name__ == "__main__":
     """ simular una fila en una entidad bancaria"""
-    pass
+    
+    customer = cliente(30311931)
+    customer.modificarcategoria("preferencial")
+    fila = FilaPreferencial()
+    fila.insertar(customer)
+    print(fila.enfila)
+    
